baae4c839ac5d8da081d597aaeb5aa460b7e2e52
Andrew Mercando <andrew.mercando@gmail.com>
2013-03-20 20:48:57 -0400

    Created Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cabbinapp::Application.config.secret_token = '045742ec4d02ff4af770be2598359cadcb772232fcfb95585df3ab033698d0a2005b4c60b5abee782402cc1f8085274d74b9eeb50c386a76f8839feab582d38b'
